fc6feab8fffeb5166a9145c92ae48c31616d3484
Nzanzu Lwanzo <nzanzu.lwanzo.work@gmail.com>
2026-06-28 01:29:45 +0200

    updates

--- src/@types.ts
+++ src/@types.ts
@@ -1,6 +1,6 @@
-
 export interface SiteProps {
-    name: string;
-    url: string;
-    description: string;
+  name: string;
+  url: string;
+  description: string;
+  idx: `0${number}`;
 }
--- src/_data.ts
+++ src/_data.ts
@@ -6,11 +6,13 @@ export const sites: SiteProps[] = [
     description:
       "Blog sur lequel j'aime parler de ce que je connais, ce que j'aime et ce que je pense.",
     url: "https://blog.lwanzo.site",
+    idx: "01",
   },
   {
     name: "Portfolio Écrivain",
     description:
       "Portfolio sur lequel sont présentés ma biographie, mes récompenses littéraires, mes projets, mes publications et mes archives",
     url: "https://art.lwanzo.site",
+    idx: "02",
   },
 ];
